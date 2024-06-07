7b67112de8959b0ab1276e35e8f54acef3dbe8b3
Arreyan Hamid <87535427+GriffinBlackbirdd@users.noreply.github.com>
2024-06-07 19:38:54 +0530

    Delete intern directory

--- intern/lib/python3.12/site-packages/fastembed/sparse/__init__.py
+++
@@ -1,4 +0,0 @@
-from fastembed.sparse.sparse_embedding_base import SparseEmbedding
-from fastembed.sparse.sparse_text_embedding import SparseTextEmbedding
-
-__all__ = ["SparseEmbedding", "SparseTextEmbedding"]
--- intern/lib/python3.12/site-packages/fastembed/text/__init__.py
+++
@@ -1,3 +0,0 @@
-from fastembed.text.text_embedding import TextEmbedding
-
-__all__ = ["TextEmbedding"]
